d61fa3e83e6aa688e6ce3a8f9e265f3a238cd8a5
DESKTOP-NAD15B0\Aditya PC <adu101992>
2017-10-07 00:33:03 +0530

    Engine Project solution setup alongwith Main and Precompiled.h. Added the SOIL (Simple OpenGL library) for testing out the linking of the include and lib folders.

---
+++ Technology/Engine/Precompiled.h
@@ -0,0 +1,26 @@
+///////////////////////////////////////////////////////////////////////////////////////
+/// All content (c) 2017 Stroblitz Engine, all rights reserved
+/// FILE: Precompiled.h
+/// DATE: 2017-10-07 / 2017-10-07
+/// AUTHOR: Aditya Subramanian <adu101992@gmail.com>
+///////////////////////////////////////////////////////////////////////////////////////
+
+#pragma once
+
+//Standard Template Libraries
+#include <algorithm>
+#include <chrono>
+#include <iostream>
+#include <fstream>
+#include <list>
+#include <map>
+#include <memory>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <queue>
+#include <unordered_map>
+#include <unordered_set>
+
+
+#include "SOIL.h"
